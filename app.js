91e9fd5d921e53ccc1b40b4e46dc4faf5289e1dc
philFernandez <norcal618@gmail.com>
2019-01-16 23:18:58 -0800

    mobile nav border

--- app.js
+++ app.js
@@ -2,15 +2,16 @@ function expandHamburger(e) {
   let mobileNav = document.getElementById("mobile-nav");
   let links = document.getElementsByTagName("a");
 
-  if (mobileNav.style.width == "100vw") {
+  if (mobileNav.style.width == "97.5vw") {
     for (link of links) {
       link.style.display = "none";
     }
-    mobileNav.style.cssText = "width: 0; height: 0;";
+    mobileNav.style.cssText = "width: 0; height: 0; border: 0";
   } else {
     for (link of links) {
       link.style.display = "block";
     }
-    mobileNav.style.cssText = "width: 100vw; height: 560px;";
+    mobileNav.style.cssText =
+      "width: 97.5vw; height: 560px; border: 5px solid black;";
   }
 }
