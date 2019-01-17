0f4c63ae7489c9b38849b7315e1dfa52d0469b0f
philFernandez <norcal618@gmail.com>
2019-01-16 19:07:28 -0800

    mobile nav works the tits; links not hooked yet

--- app.js
+++ app.js
@@ -6,14 +6,11 @@ function expandHamburger(e) {
     for (link of links) {
       link.style.display = "none";
     }
-
-    mobileNav.style.width = "0";
-    mobileNav.style.height = "0";
+    mobileNav.style.cssText = "width: 0; height: 0;";
   } else {
     for (link of links) {
       link.style.display = "block";
     }
-    mobileNav.style.width = "75%";
-    mobileNav.style.height = "700px";
+    mobileNav.style.cssText = "width: 75%; height: 560px;";
   }
 }
