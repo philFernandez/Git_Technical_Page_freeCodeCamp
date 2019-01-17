4eda5dbc9e5d50d27485c4e7fef6283b237f99ee
philFernandez <norcal618@gmail.com>
2019-01-16 18:45:33 -0800

    add links to mobile nav

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
-var hasBeenClicked = false;
-
 function expandHamburger(e) {
   let mobileNav = document.getElementById("mobile-nav");
-  if (!hasBeenClicked) {
-    mobileNav.style.width = "75%";
-    mobileNav.style.height = "700px";
-    hasBeenClicked = true;
-  } else {
+  let links = document.getElementsByTagName("a");
+
+  if (mobileNav.style.width == "75%") {
+    for (link of links) {
+      link.style.display = "none";
+    }
+
     mobileNav.style.width = "0";
     mobileNav.style.height = "0";
-    hasBeenClicked = false;
+  } else {
+    for (link of links) {
+      link.style.display = "block";
+    }
+    mobileNav.style.width = "75%";
+    mobileNav.style.height = "700px";
   }
 }
