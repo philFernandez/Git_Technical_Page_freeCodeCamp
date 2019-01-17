6262073405b07e65163c69b386f48be500009bfd
philFernandez <norcal618@gmail.com>
2019-01-17 00:50:53 -0800

    comments

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 function expandHamburger(e) {
+  // get the div which hold the links
   let mobileNav = document.getElementById("mobile-nav");
+  // get the links themselves
   let links = document.getElementsByTagName("a");
 
+  // if the navigation menu is already being shown, then hide it
   if (mobileNav.style.width == "97.5vw") {
     for (link of links) {
       link.style.display = "none";
     }
     mobileNav.style.cssText = "width: 0; height: 0; border: 0";
+
+    // else if the navigation menu is hidden then show it
   } else {
     for (link of links) {
       link.style.display = "block";
