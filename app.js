34a84e383363261a970eab3b70f1235f0bc7bb0f
philFernandez <norcal618@gmail.com>
2019-01-18 14:31:10 -0800

    nav bar seems to be hiding

--- app.js
+++ app.js
@@ -1,35 +1,54 @@
-function expandHamburger(e) {
-  // get the div which hold the links
-  let mobileNav = document.getElementById("mobile-nav");
-  // get the links themselves
-  // let links = document.getElementsByTagName("a");
+// expandHamburger
+(function() {
+  let navbar = document.getElementById("navbar");
+  navbar.addEventListener("click", expandHamburger);
+
+  function expandHamburger() {
+    // get the div which hold the links
+    let mobileNav = document.getElementById("mobile-nav");
+    // get the links themselves
+    // let links = document.getElementsByTagName("a");
+    let links = document.getElementsByClassName("nav-link");
+
+    // if the navigation menu is already being shown, then hide it
+    if (mobileNav.style.width == "97.5vw") {
+      for (let link of links) {
+        link.style.display = "none";
+      }
+      mobileNav.style.cssText = "width: 0; height: 0; border: 0";
+
+      // else if the navigation menu is hidden then show it
+    } else {
+      for (let link of links) {
+        link.style.display = "block";
+      }
+      mobileNav.style.cssText =
+        "width: 97.5vw; height: 56rem; border: 0.5rem solid black;";
+    }
+  }
+})();
+
+(function() {
+  let navBar = document.getElementById("navbar");
+  window.addEventListener("scroll", () => {
+    // console.log(this.oldScroll < this.scrollY);
+    if (this.oldScroll < this.scrollY) {
+      navBar.style.display = "flex";
+    } else {
+      // navBar.style.display = "none";
+    }
+
+    this.oldScroll = this.scrollY;
+  });
+
   let links = document.getElementsByClassName("nav-link");
-
-  // if the navigation menu is already being shown, then hide it
-  if (mobileNav.style.width == "97.5vw") {
-    for (link of links) {
-      link.style.display = "none";
-    }
-    mobileNav.style.cssText = "width: 0; height: 0; border: 0";
-
-    // else if the navigation menu is hidden then show it
-  } else {
-    for (link of links) {
-      link.style.display = "block";
-    }
-    mobileNav.style.cssText =
-      "width: 97.5vw; height: 56rem; border: 0.5rem solid black;";
+  for (let link of links) {
+    link.addEventListener("click", hideNav);
   }
-}
 
-let navigationBar = document.getElementById("navbar");
-function handleNav(e) {
-  if (navigationBar.style["display"] === "flex") {
-    navigationBar.style.display = "none";
-  } else {
-    navigationBar.style.display = "flex";
+  function hideNav() {
+    setTimeout(function() {
+      navBar.style.display = "none";
+    }, 60);
   }
-  console.log(navigationBar.style["display"]);
-}
-
-window.addEventListener("click", handleNav);
+})();
