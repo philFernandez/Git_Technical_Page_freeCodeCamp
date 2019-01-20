1181d5d157cd2150750c06a7765e81bf6f7b9177
philFernandez <norcal618@gmail.com>
2019-01-20 01:19:38 -0800

    changed from < 768 to <= 768

--- app.js
+++ app.js
@@ -68,10 +68,11 @@
 // the nav's display property is set to flex. Else if we
 // are in desktop mode ie > 768px wide, the nav's display
 // property is in block
+
 (function() {
   let navBar = document.getElementById("navbar");
   window.addEventListener("resize", e => {
-    if (e.target.innerWidth > 768) {
+    if (e.target.innerWidth >= 768) {
       navBar.style.display = "block";
     } else {
       navBar.style.display = "flex";
