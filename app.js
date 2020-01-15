fb9b08d7f8e4143aec96747ae8cee2228d54ad0c
philFernandez <norcal618@gmail.com>
2020-01-15 03:39:15 -0800

    cobbled back together files that work

--- app.js
+++ app.js
@@ -4,42 +4,55 @@
   navbar.addEventListener("click", expandHamburger);
 
   function expandHamburger() {
-    // get the div which hold the links
-    let mobileNav = document.getElementById("mobile-nav");
-    // get the links themselves
-    // let links = document.getElementsByTagName("a");
-    let links = document.getElementsByClassName("nav-link");
+    let viewPortWidth = window.innerWidth;
+    // make sure we are in mobile mode before doing any of the mobile nav things
+    if (viewPortWidth <= 768) {
+      // get the div which hold the links
+      let mobileNav = document.getElementById("mobile-nav");
+      // get the links themselves
+      // let links = document.getElementsByTagName("a");
+      let links = document.getElementsByClassName("nav-link");
 
-    // if the navigation menu is already being shown, then hide it
-    if (mobileNav.style.width == "97.5vw") {
-      for (let link of links) {
-        link.style.display = "none";
-      }
-      mobileNav.style.cssText = "width: 0; height: 0; border: 0";
+      // if the navigation menu is already being shown, then hide it
+      let mobileNavWidth = "100vw"; // this is the width of the dropdown mobile nav
+      if (mobileNav.style.width == mobileNavWidth) {
+        for (let link of links) {
+          link.style.display = "none";
+        }
+        mobileNav.style.cssText = "width: 0; height: 0; border: 0";
 
-      // else if the navigation menu is hidden then show it
-    } else {
-      for (let link of links) {
-        link.style.display = "block";
+        // else if the navigation menu is hidden then show it
+      } else {
+        for (let link of links) {
+          link.style.display = "block";
+        }
+
+        mobileNav.style.cssText =
+          "width: " +
+          mobileNavWidth +
+          "; height: 56rem; border-bottom: 0.2rem solid lightgrey;";
       }
-      mobileNav.style.cssText =
-        "width: 97.5vw; height: 56rem; border: 0.5rem solid black;";
     }
   }
 })();
 
+// hide nav when scolling down; show nav when scolling up
 (function() {
   let navBar = document.getElementById("navbar");
   window.addEventListener("scroll", () => {
-    // if scrolling down, hide the navbar
-    if (this.oldScroll < this.scrollY) {
-      navBar.style.display = "none";
-    } else {
-      // if scrolling up, show the nav bar
-      navBar.style.display = "flex";
-    }
+    let viewPortWidth = window.innerWidth;
+    // make sure we are in mobile mode before doing any of the mobile nav things
+    if (viewPortWidth <= 768) {
+      // if scrolling down, hide the navbar
+      if (this.oldScroll < this.scrollY) {
+        navBar.style.display = "none";
+      } else {
+        // if scrolling up, show the nav bar
+        navBar.style.display = "flex";
+      }
 
-    this.oldScroll = this.scrollY;
+      this.oldScroll = this.scrollY;
+    }
   });
 
   let links = document.getElementsByClassName("nav-link");
@@ -49,8 +62,39 @@
 
   // hide the nav bar when selecting a link
   function hideNav() {
-    setTimeout(function() {
-      navBar.style.display = "none";
-    }, 60);
+    let viewPortWidth = window.innerWidth;
+    if (viewPortWidth <= 768) {
+      setTimeout(() => {
+        navBar.style.display = "none";
+      }, 60);
+    }
+  }
+})();
+
+// resize listener with throttling built in
+(function() {
+  let mobileNav = document.getElementById("mobile-nav");
+  let navbar = document.getElementById("navbar");
+  window.addEventListener("resize", resizeThrottler, false);
+
+  var resizeTimeout;
+  function resizeThrottler(e) {
+    // ignore events as long as an actualResizeHandler execution is in the queue
+    if (!resizeTimeout) {
+      resizeTimeout = setTimeout(() => {
+        resizeTimeout = null;
+        actualResizeHandler(e);
+      }, 66);
+    }
+  }
+
+  // this is where you do the things
+  function actualResizeHandler(e) {
+    if (e.target.innerWidth > 768) {
+      mobileNav.style.display = "none";
+      navbar.style.display = "none";
+    } else {
+      navbar.style.display = "flex";
+    }
   }
 })();
